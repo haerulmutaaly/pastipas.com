be31cfb5292a61493e6c159f0ad1e99d9fe24cb0
haerulmutaaly <haerulmutaaly@gmail.com>
2017-10-25 19:13:19 +0700

    Update Pemanggil.js

--- Pemanggil.js
+++ Pemanggil.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*20)+ 1);var xxx=null;
 if(x=="1"){xxx="/2017/05/gitar-akustik.html"}
 if(x=="2"){xxx="/2017/05/gitar-akustik.html"}
-if(x=="3"){xxx="/2016/10/cara-merawat-kulit-wajah-berminyak.html"}
-if(x=="4"){xxx="/2016/10/cara-merawat-kulit-wajah-berminyak.html"}
-if(x=="5"){xxx="/2016/10/cara-merawat-kulit-wajah-berminyak.html"}
-if(x=="6"){xxx="/2016/10/cara-merawat-kulit-wajah-berminyak.html"}
+if(x=="3"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
+if(x=="4"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
+if(x=="5"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
+if(x=="6"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
 if(x=="7"){xxx="/2016/10/cara-merawat-kulit-wajah-berminyak.html"}
 if(x=="8"){xxx="/2016/10/cara-merawat-kulit-wajah-berminyak.html"}
 if(x=="9"){xxx="/2016/10/cara-merawat-kulit-wajah-berminyak.html"}
@@ -36,9 +36,9 @@ if(x=="14"){xxx="/2017/06/10-latest-games-2017.html"}
 if(x=="15"){xxx="/2017/06/10-latest-games-2017.html"}
 if(x=="16"){xxx="/2017/06/10-latest-games-2017.html"}
 if(x=="17"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="18"){xxx="/2017/05/gitar-akustik.html"}
-if(x=="19"){xxx="/2017/05/gitar-akustik.html"}
-if(x=="20"){xxx="/2017/05/gitar-akustik.html"}a_to_ve[i].href="https://www.pastipas.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="18"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
+if(x=="19"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
+if(x=="20"){xxx="/2017/08/6-best-android-camera-smartphone.html"}a_to_ve[i].href="https://www.pastipas.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
