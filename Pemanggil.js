eb4a52a9e5a3e5ae806d70e01a7d0a837186a36c
haerulmutaaly <haerulmutaaly@gmail.com>
2018-07-19 12:04:42 +0700

    Update Pemanggil.js

--- Pemanggil.js
+++ Pemanggil.js
@@ -25,20 +25,20 @@ if(x=="3"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
 if(x=="4"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
 if(x=="5"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
 if(x=="6"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
-if(x=="7"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="8"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="9"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="10"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="11"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="12"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="13"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="14"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="15"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="16"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="17"){xxx="/2017/06/10-latest-games-2017.html"}
-if(x=="18"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
-if(x=="19"){xxx="/2017/08/6-best-android-camera-smartphone.html"}
-if(x=="20"){xxx="/2017/08/6-best-android-camera-smartphone.html"}a_to_ve[i].href="https://www.pastipas.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="7"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="8"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="9"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="10"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="11"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="12"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="13"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="14"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="15"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="16"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="17"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="18"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="19"){xxx="/2018/05/popular-new-car-2019.html"}
+if(x=="20"){xxx="/2018/05/popular-new-car-2019.html"}a_to_ve[i].href="https://www.pastipas.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
